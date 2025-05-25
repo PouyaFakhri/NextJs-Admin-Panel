f0c13cc791d83b1fdef92287b44e7cf374ae050a
PouyaFakhri <pouyaf77.pf@gmail.com>
2025-05-25 11:35:50 +0330

    add dashboard page structure

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,8 +1,54 @@
+import { CiSearch } from "react-icons/ci";
+import { BsPersonCircle } from "react-icons/bs";
+import { IoIosLogOut } from "react-icons/io";
+import Image from "next/image";
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
-  )
+    <div>
+      <div>
+        <CiSearch size={24} />
+        <input type="search" placeholder=" جستجو کالا" />
+        <div>
+          <BsPersonCircle size={35} />
+          <div>
+            <h5>pouyaf98</h5>
+            <p>مدیر</p>
+          </div>
+        </div>
+        <div>
+          <IoIosLogOut size={25} />
+          <p>خروج</p>
+        </div>
+      </div>
+      <div>
+        <div>
+          <Image
+            src="/images/setting-3.png"
+            alt="مدیریت کالا"
+            width={30}
+            height={30}
+          />
+          <p>مدیریت کالا</p>
+        </div>{" "}
+        <button type="submit">افزودن محصول </button>
+      </div>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th> نام کالا</th>
+              <th> موجودی </th>
+              <th> قیمت</th>
+              <th> شناسه کالا </th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody></tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
