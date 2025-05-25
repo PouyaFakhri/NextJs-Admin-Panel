c768bc78b502a6d614be37e572079f4656fbac6d
PouyaFakhri <pouyaf77.pf@gmail.com>
2025-05-25 11:51:53 +0330

    add dashboard page style

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -2,26 +2,27 @@ import { CiSearch } from "react-icons/ci";
 import { BsPersonCircle } from "react-icons/bs";
 import { IoIosLogOut } from "react-icons/io";
 import Image from "next/image";
+import styles from "../styles/Dashboard.module.css"
 
 function Dashboard() {
   return (
-    <div>
-      <div>
+    <div  className={styles.container}>
+      <div className={styles.header}>
         <CiSearch size={24} />
         <input type="search" placeholder=" جستجو کالا" />
-        <div>
-          <BsPersonCircle size={35} />
+        <div className={styles.headersprof}>
+          <BsPersonCircle size={35}  />
           <div>
             <h5>pouyaf98</h5>
             <p>مدیر</p>
           </div>
         </div>
-        <div>
-          <IoIosLogOut size={25} />
+        <div className={styles.headerslog}>
+          <IoIosLogOut size={25} className={styles.logout}/>
           <p>خروج</p>
         </div>
       </div>
-      <div>
+      <div className={styles.addproduct}>
         <div>
           <Image
             src="/images/setting-3.png"
@@ -33,15 +34,15 @@ function Dashboard() {
         </div>{" "}
         <button type="submit">افزودن محصول </button>
       </div>
-      <div>
+      <div className={styles.productManagement}>
         <table>
-          <thead>
-            <tr>
+          <thead className={styles.thead}>
+            <tr className={styles.tabelHeader}>
               <th> نام کالا</th>
               <th> موجودی </th>
               <th> قیمت</th>
               <th> شناسه کالا </th>
-              <th></th>
+              <th className={styles.options}></th>
             </tr>
           </thead>
           <tbody></tbody>
